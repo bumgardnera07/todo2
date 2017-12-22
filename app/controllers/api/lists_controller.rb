60f903b198606d29e49eff600557daaba7e43c62
George Bumgardner <bumgardnera07@gmail.com>
2017-12-22 02:23:50 +0000

    destroy implemented

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -35,7 +35,13 @@ class Api::ListsController < ApiController
 
   # DELETE /lists/1
   def destroy
-    @list.destroy
+    begin
+       list = List.find(params[:id])
+       list.destroy
+       render json: {}, status: :no_content
+     rescue ActiveRecord::RecordNotFound
+       render :json => {}, :status => :not_found
+    end
   end
 
   private
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -35,7 +35,13 @@ class Api::UsersController < ApiController
 
   # DELETE /users/1
   def destroy
-    @user.destroy
+    begin
+       user = User.find(params[:id])
+       user.destroy
+       render json: {}, status: :no_content
+     rescue ActiveRecord::RecordNotFound
+       render :json => {}, :status => :not_found
+    end
   end
 
   private
