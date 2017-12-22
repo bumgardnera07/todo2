546dd6f8dba9fe4bc5d1c8f61300170e570efff3
George Bumgardner <bumgardnera07@gmail.com>
2017-12-22 00:09:45 +0000

    fixed usernames and Action Controller associations

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,4 @@
 class ApiController < ApplicationController
-    skip_before_action :verify_authenticity_token
     
     private
     def authenticated?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214034156) do
+ActiveRecord::Schema.define(version: 20171222000505) do
 
   create_table "items", force: :cascade do |t|
     t.string "text"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20171214034156) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
